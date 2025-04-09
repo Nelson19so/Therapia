1a8c2b461626a8df29928851a5c20d684d76eb13
Nelson19so <nelsonsomto10@gmail.com>
2025-04-09 19:06:57 +0100

    clean serializer

--- server/apps/authentication/serializers.py
+++ server/apps/authentication/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
+from .models import CustomUser
 
 User = get_user_model()
 
@@ -10,26 +10,50 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['id', 'email', 'phone_number', 'first_name', 'last_name', 'terms_accepted']
         read_only_fields = ['id']
-        
+
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     confirm_password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = CustomUser
-        fields = ['email', 'phone_number', 'first_name', 'last_name', 'password', 'confirm_password', 'terms_accepted']
-        
+        fields = [
+            'email', 'phone_number', 'first_name', 'last_name',
+            'password', 'confirm_password', 'terms_accepted'
+        ]
+
     def validate(self, attrs):
+        first_name = attrs.get('first_name')
+        last_name = attrs.get('last_name')
+        email = attrs.get('email')
+        phone_number = attrs.get("phone_number")
+
+        # ✅ filter conf
+        if CustomUser.objects.filter(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            phone_number=phone_number
+        ).exists():
+            raise serializers.ValidationError({
+                'user': "A user with this information already exists."
+            })
+
+        # ✅ Password confirmation
         if attrs['password'] != attrs['confirm_password']:
-            raise serializers.ValidationError({"password": "Password and Confirm Password must be same"})
-        
+            raise serializers.ValidationError({
+                "password": "Password and Confirm Password must match."
+            })
+
+        # ✅ Terms check
         if not attrs.get('terms_accepted', False):
-            raise serializers.ValidationError({"terms_accepted": "Please accept the terms and conditions."})
-        
+            raise serializers.ValidationError({
+                "terms_accepted": "Please accept the terms and conditions."
+            })
+
         return attrs
-    
+
     def create(self, validated_data):
-        # This is to remove the field confirm password because we don't need it to be stored in the database
-        validated_data.pop('confirm_password')
-        
+        validated_data.pop('confirm_password')  # Don't store confirm_password
         user = CustomUser.objects.create_user(**validated_data)
         return user
