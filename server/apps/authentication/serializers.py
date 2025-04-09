0f708392164d79cf2fc809a901f5813f9aa5e9e2
Timothy Dawadakpoye <timdawadakpoye@gmail.com>
2025-04-09 16:39:59 +0100

    Create serializers.py
    Worked on the serializers to signup users

---
+++ server/apps/authentication/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import CustomUser
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'phone_number', 'first_name', 'last_name', 'terms_accepted']
+        read_only_fields = ['id']
+        
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    confirm_password = serializers.CharField(write_only=True, required=True)
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'phone_number', 'first_name', 'last_name', 'password', 'confirm_password', 'terms_accepted']
+        
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({"password": "Password and Confirm Password must be same"})
+        
+        if not attrs.get('terms_accepted', False):
+            raise serializers.ValidationError({"terms_accepted": "Please accept the terms and conditions."})
+        
+        return attrs
+    
+    def create(self, validated_data):
+        # This is to remove the field confirm password because we don't need it to be stored in the database
+        validated_data.pop('confirm_password')
+        
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
