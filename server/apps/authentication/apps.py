ec8783555eee21b0cd6b3325457bea8dde83e4c6
Nelson <nelsonsomto10@gmail.com>
2025-05-01 09:39:27 +0100

    db correction

--- server/apps/authentication/apps.py
+++ server/apps/authentication/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.authentication'
-    label = 'accounts'
+    label = 'authentication'      # This is the app label used in AUTH_USER_MODEL
