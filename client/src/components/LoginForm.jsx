4333cf41fcada5d53dd4d490e12d92908e451cc3
JimOputa <85954599+JimOputa@users.noreply.github.com>
2025-05-02 09:57:33 +0100

    Connect frontend to API register endpoint

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
 import FormInput from "./FormInput";
 
 export default function LoginForm() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [inputError, setInputError] = useState({
+    email: "",
+    password: "",
+  });
   return (
     <form
       action=""
       className="flex flex-col gap-y-[11px]"
       onSubmit={(e) => e.preventDefault()}
     >
-      <FormInput type="email" placeholder="Email" name="email" />
-      <FormInput type="password" placeholder="Password" autoComplete="off" />
+      <FormInput
+        type="text"
+        placeholder="Email"
+        name="email"
+        formData={formData}
+        setFormData={setFormData}
+        inputError={inputError}
+      />
+      <FormInput
+        type="password"
+        placeholder="Password"
+        autoComplete="off"
+        formData={formData}
+        setFormData={setFormData}
+        inputError={inputError}
+      />
       <button className="sm:text-[17px]/[35px] text-[14px]/[35px] text-[var(--text-light)] font-bold bg-[var(--primary)] px-[19px] py-[10px] rounded-[10px]">
         Login
       </button>
