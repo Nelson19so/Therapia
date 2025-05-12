a6de729d487c188dc8d8d241a3c9769231adb045
Nelson <nelsonsomto10@gmail.com>
2025-05-13 00:31:21 +0100

    Update settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -88,7 +88,9 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 # cors headers configuration
 
-CORS_ALLOWED_ORIGINS = True
+CORS_ALLOWED_ORIGINS = {
+    "*"
+}
 
 CORS_ALLOW_METHODS = (
     "DELETE",
