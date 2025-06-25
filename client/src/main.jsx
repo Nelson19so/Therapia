a77d94b472dc5afaf3502a22d85db0df7999ec0b
goodluck1455 <goodluckstephen237@gmail.com>
2025-06-25 16:42:56 +0100

    Routing update in progress

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,41 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HeaderAndFooter from "./layouts/HeaderAndFooter.jsx";
 import Landing from "./pages/Landing.jsx";
 import ScheduleSession from "./pages/ScheduleSession.jsx";
 import Signup from "./pages/Signup.jsx";
 import Login from "./pages/Login.jsx";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        {/* Layout Routes with Header and Footer */}
-        <Route element={<HeaderAndFooter />}>
-          <Route index element={<Landing />} />
-          <Route path="schedule-session" element={<ScheduleSession />} />
-        </Route>
-        <Route path="signup" element={<Signup />} />
-        <Route path="login" element={<Login />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
+
+
+
+
+export default function App() {
+  return (
+    <>
+    <Header />
+      <Routes>
+        {/* Layout Routes with Header and Footer */}
+        <Route index element={<Landing />} />
+         <Route path="schedule-session" element={<ScheduleSession />} />
+          <Route path="*" element={<NotFound />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+      <Footer />
+      
+    </>
+  )
+}
+
